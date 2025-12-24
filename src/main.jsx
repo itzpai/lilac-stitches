aa75b5a911703fe048e0c81d6a314d9a301c25e5
Paing Thu Aung <paing.pta@gmail.com>
2025-12-24 14:41:05 +0630

    add layout for header and footer

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import './index.css'
+import Layout from './components/Layout'
 import Home from './pages/Home'
 import Shop from './pages/Shop'
 import ProductDetails from './pages/ProductDetails'
@@ -14,31 +15,42 @@ import Faq from './pages/Faq'
 
 const router = createBrowserRouter([{
   path: "/",
-  element: <Home />,
-  errorElement: <Error />
-}, {
-  path: "/shop",
-  element: <Shop />,
-}, {
-  path: "/product/:id",
-  element: <ProductDetails />
-}, {
-  path: "/aboutus",
-  element: <AboutUs />
-}, {
-  path: "/contactus",
-  element: <ContactUs />
-}, {
-  path: "/cart",
-  element: <Cart />
-}, {
-  path: "/privacypolicy",
-  element: <PrivacyPolicy />
-}, {
-  path: "/faqs",
-  element: <Faq />
-}
-])
+  element: <Layout />,
+  errorElement: <Error />,
+  children: [{
+    index: true,
+    element: <Home />
+  },
+  {
+    path: "shop",
+    element: <Shop />
+  },
+  {
+    path: "product/:id",
+    element: <ProductDetails />
+  },
+  {
+    path: "aboutus",
+    element: <AboutUs />
+  },
+  {
+    path: "contactus",
+    element: <ContactUs />
+  },
+  {
+    path: "cart",
+    element: <Cart />
+  },
+  {
+    path: "privacypolicy",
+    element: <PrivacyPolicy />
+  },
+  {
+    path: "faqs",
+    element: <Faq />
+  }
+  ]
+}])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
