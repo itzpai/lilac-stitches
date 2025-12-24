f9e13d2cdb6c5586e1ec9998372797d084df3ab9
Paing Thu Aung <paing.pta@gmail.com>
2025-12-24 13:43:13 +0630

    base project with react-router setup

---
+++ src/main.jsx
@@ -0,0 +1,47 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import './index.css'
+import Home from './pages/Home'
+import Shop from './pages/Shop'
+import ProductDetails from './pages/ProductDetails'
+import Error from './pages/Error'
+import AboutUs from './pages/AboutUs'
+import ContactUs from './pages/ContactUs'
+import Cart from './pages/Cart'
+import PrivacyPolicy from './pages/PrivacyPolicy'
+import Faq from './pages/Faq'
+
+const router = createBrowserRouter([{
+  path: "/",
+  element: <Home />,
+  errorElement: <Error />
+}, {
+  path: "/shop",
+  element: <Shop />,
+}, {
+  path: "/product/:id",
+  element: <ProductDetails />
+}, {
+  path: "/aboutus",
+  element: <AboutUs />
+}, {
+  path: "/contactus",
+  element: <ContactUs />
+}, {
+  path: "/cart",
+  element: <Cart />
+}, {
+  path: "/privacypolicy",
+  element: <PrivacyPolicy />
+}, {
+  path: "/faqs",
+  element: <Faq />
+}
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
